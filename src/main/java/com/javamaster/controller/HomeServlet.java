b776d4fb4f8e41121ffd2ae9f32a2d4f888f0222
JuanGQCadavid <juan.gonzalo@comono.co>
2020-10-05 22:03:55 -0500

    CHanging repo

--- src/main/java/com/javamaster/controller/HomeServlet.java
+++ src/main/java/com/javamaster/controller/HomeServlet.java
@@ -1,58 +1,43 @@
 package com.javamaster.controller;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.javamaster.dao.ArticleDao;
-import com.javamaster.dao.impl.ArticleDaoImpl;
-import com.javamaster.entity.Article;
-import com.javamaster.entity.Category;
 
 /**
  * Servlet implementation class HomeServlet
  */
 public class HomeServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	
-	private ArticleDao articleDao = new ArticleDaoImpl();
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
+    private static final long serialVersionUID = 1L;
+    
     public HomeServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String path = request.getServletPath();
-		if (path.equals("/")){
-			List<Article> articles = articleDao.getAllArticles();
-			request.setAttribute("title", "Title of our application");
-			request.setAttribute("articles", articles);
-			request.getRequestDispatcher("/WEB-INF/view/index.jsp").forward(request, response);
-		}
-		
-		else if (path.equals("/welcome")){
-			request.getRequestDispatcher("/WEB-INF/view/welcome.jsp").forward(request, response);
-		}
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path = request.getServletPath();
+        if (path.equals("/")) {
+            response.setContentType("application/xml");
+            response.getWriter().println("<Response>Hola Servlet</Response>");
+        }
+        if (path.equals("/1")) {
+            response.sendRedirect("http://cloud.zang.io/data/inboundxml/3ce76e1a353a539f25a99a8ebf0f98db8ec6e83f");
+        }
+        if (path.equals("/2")) {
+            response.sendRedirect("http://cloud.zang.io/data/inboundxml/70bea44d2df496e06566e4122637d430d1a73011");
+        }
+    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       response.getWriter().println("Hello");
+    }
 
 }
